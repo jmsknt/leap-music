e1b6f364ba64da88d3b8f9209ff5704ecb0e2045
James Kent <jamesalexkent@gmail.com>
2019-03-31 12:13:37 +0100

    remove junk file

--- src/js/theremin.js
+++ src/js/theremin.js
@@ -29,47 +29,50 @@ reverb = new p5.Reverb();
 reverb.process(primaryOsc, 1, 2);
 reverb.process(primaryOscPartner, 1, 5);
 
+primaryOsc.amp(0.6);
+primaryOsc.freq(523.25);
+primaryOsc.start();
 
-// main loop
-Leap.loop(leapOptions, function (frame) {
+// // main loop
+// Leap.loop(leapOptions, function (frame) {
 
-    // Display Frame object data
-    frameOutput = document.getElementById("frameData");
+//     // Display Frame object data
+//     frameOutput = document.getElementById("frameData");
 
-    output = "Frame ID: " + frame.id + "<br />"
-        + "Timestamp: " + frame.timestamp + " &micro;s<br />"
+//     output = "Frame ID: " + frame.id + "<br />"
+//         + "Timestamp: " + frame.timestamp + " &micro;s<br />"
     
 
-    if (frame.hands.length > 0) {
-        primaryHand = frame.hands[0];
+//     if (frame.hands.length > 0) {
+//         primaryHand = frame.hands[0];
         
-        // P5 STUFF
+//         // P5 STUFF
 
-        primaryFrequency = 5 * primaryHand.palmPosition[1];
-        primaryOsc.freq(primaryFrequency);
-        primaryOscPartner.freq(primaryFrequency);
+//         primaryFrequency = 5 * primaryHand.palmPosition[1];
+//         primaryOsc.freq(primaryFrequency);
+//         primaryOscPartner.freq(primaryFrequency);
     
 
-        amplitude = 1-(primaryHand.grabStrength);
-        primaryOsc.amp(amplitude);
-        primaryOscPartner.amp(0);
+//         amplitude = 1-(primaryHand.grabStrength);
+//         primaryOsc.amp(amplitude);
+//         primaryOscPartner.amp(0);
 
-        output += "<br />" + "Hand detected, play sound!" + "<br />"
-        + "Primary hand position X: " + (primaryHand.palmPosition[0]) + "<br />"
-        + "Primary hand position Y: " + (primaryHand.palmPosition[1]) + "<br />"
-        + "Primary hand position Z: " + (primaryHand.palmPosition[2]) + "<br />"
-        + "Primary hand amplitude: " + (amplitude) + "<br />"
-        + "Primary hand frequency: " + Math.round(primaryFrequency).toFixed(2) + "<br />";
+//         output += "<br />" + "Hand detected, play sound!" + "<br />"
+//         + "Primary hand position X: " + (primaryHand.palmPosition[0]) + "<br />"
+//         + "Primary hand position Y: " + (primaryHand.palmPosition[1]) + "<br />"
+//         + "Primary hand position Z: " + (primaryHand.palmPosition[2]) + "<br />"
+//         + "Primary hand amplitude: " + (amplitude) + "<br />"
+//         + "Primary hand frequency: " + Math.round(primaryFrequency).toFixed(2) + "<br />";
     
-    }
-    else {
-        primaryOsc.amp(0.0);
-        primaryOscPartner.amp(0.0);    
+//     }
+//     else {
+//         primaryOsc.amp(0.0);
+//         primaryOscPartner.amp(0.0);    
 
-    }
-    frameOutput.innerHTML = "<div>" + output + "</div>";
+//     }
+//     frameOutput.innerHTML = "<div>" + output + "</div>";
 
-    // Store frame for motion functions
-    previousFrame = frame;
+//     // Store frame for motion functions
+//     previousFrame = frame;
 
-})
+// })
