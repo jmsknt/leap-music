303646961100c5651e9874ca77ecac2d36d8f820
James Kent <jamesalexkent@gmail.com>
2019-04-28 15:11:21 +0100

    tweaks

--- js/theremin.js
+++ js/theremin.js
@@ -36,7 +36,7 @@ f_osc3.start();
 f_osc4.start();
 
 // main loop
-Leap.loop(leapOptions, function (frame) {
+Leap.loop(leapOptions, function (frame,) {
 
     // Display Frame object data
     frameOutput = document.getElementById("frameData");
@@ -47,20 +47,20 @@ Leap.loop(leapOptions, function (frame) {
 
     if (frame.hands.length > 0) {
 
-
         hand = frame.hands[0];
         handPosition = hand.palmPosition;
         
-        osc.amp(primary_vol);
-        f_osc.amp(harmonics_vol)/0.33; // third harmonic
-        f_osc2.amp(harmonics_vol)/0.20; // fifth harmonic
-        f_osc3.amp(harmonics_vol)/0.14; // seventh harmonic
-        f_osc4.amp(harmonics_vol)/0.11; // ninth harmonic
+        osc.amp(primary_vol); // master
+        f_osc.amp((harmonics_vol)/0.33); // third harmonic
+        f_osc2.amp((harmonics_vol)/0.20); // fifth harmonic
+        f_osc3.amp((harmonics_vol)/0.14); // seventh harmonic
+        f_osc4.amp((harmonics_vol)/0.11); // ninth harmonic
 
         if (handPosition[1] > 350) {
             primary_vol = 0.0;    
         }
         else {
+
             // scale frequency into 4th octave (middle octave)
             frequency = ((523.25 -  261.63) * handPosition[1]-30) / (300) +  261.63;
             osc.freq(frequency);
@@ -84,7 +84,12 @@ Leap.loop(leapOptions, function (frame) {
 
     }
     else {
-        primary_vol = 0.0;
+        osc.amp(0); // master
+        f_osc.amp(0); // third harmonic
+        f_osc2.amp(0); // fifth harmonic
+        f_osc3.amp(0); // seventh harmonic
+        f_osc4.amp(0); // ninth harmonic
+
     }
     frameOutput.innerHTML = "<div>" + output + "</div>";
 
