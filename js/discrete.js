58798f9682a5d987196dd982070c1f2a5938b8cc
James Kent <jamesalexkent@gmail.com>
2019-05-01 04:57:43 +0100

    final push before hand in

--- js/discrete.js
+++ js/discrete.js
@@ -1,99 +1,106 @@
-var previousFrame = null;
-var output = null;
+var output = "";
 var leapOptions = {enableGestures: false};
-var currentFreq = 0;
-var primaryAmplitude = 0.0;
+var primary_freq = 0;
+var secondary_freq = 0;
+var primary_amp = 0;
+var secondary_amp = 0;
 
-var primaryOsc = new p5.Oscillator();
-primaryOsc.setType('square');
-primaryOsc.amp(primaryAmplitude)
-primaryOsc.start();
+// primary hand base oscillator
+var b_osc = new p5.Oscillator();
+b_osc.setType('sine');
+b_osc.amp(primary_amp);
+b_osc.start();
 
-// notes are ordered by octave and frequency
-var notes = [
-    // ocatave 0: C0, D0, E0, F0, G0, A0, B0
-    [16.35,18.35,20.60,21.83,24.50,27.50,30.87],
-    // ocatave 1: C1, D1, E1, F1, G1, A1, B1
-    [32.70,36.71,41.20,43.65,49.00,55.00,61.74],
-    // ocatave 2: C2, D2, E2, F2, G2, A2, B2
-    [64.41,73.42,82.41,87.31,98.00,110.00,123.47],
-    // ocatave 3: C3, D3, E3, F3, G3, A3, B3
-    [130.81,146.83,164.81,174.61,190.00,220.00,246.94],
-    // ocatave 4: C4, D4, E4, F4, G4, A4, B4
-    [261.63,293.66,329.63,349.23,392.00,440.00,493.88],
-    // ocatave 5: C5, D5, E5, F5, G5, A5, B5
-    [523.25,587.33,659.25,698.25,783.99,880.00,987.77],
-    // ocatave 6: C6, D6, E6, F6, G6, A6, B6
-    [1046.50,1174.66,1318.51,1396.91,1567.98,1760.00,1975.53]
-];
+// primary hand 2nd harmonic 
+var h_osc = new p5.Oscillator();
+h_osc.setType('sine');
+h_osc.amp(primary_amp);
+h_osc.start();
+
+// secondary hand base oscillator
+var b_osc2 = new p5.Oscillator();
+b_osc2.setType('sine')
+b_osc2.amp(secondary_amp);
+b_osc2.start();
+
+// secondary hand 2nd harmonic
+var h_osc2 = new p5.Oscillator();
+h_osc2.setType('sine');
+h_osc2.amp(secondary_amp);
+h_osc2.start();
 
 Leap.loop(leapOptions, function(frame) {
 
     // Display Frame object data
     var frameOutput = document.getElementById("frameData");
 
-    var output = "Frame ID: " + frame.id  + "<br />"
-                    + "Timestamp: " + frame.timestamp + " &micro;s<br />"
-
     if (frame.hands.length > 0) {
             
-        var hand = frame.hands[0];
-        primaryOsc.amp(0.1);
+        var hand_one = frame.hands[0];
+        var osc_one = handOscillator(output, hand_one, b_osc, h_osc, primary_amp, primary_freq);
 
-        if (hand.grabStrength < 0.8) {
-            output += "Hand detected, play sound!" + "<br />"
-            + "Freq: " + currentFreq + "<br />"
-            + "Palm position X: " + (hand.palmPosition[0]) + "<br />"
-            + "Palm position Y: " + (hand.palmPosition[1]) + "<br />"
-            + "Palm position Z: " + (hand.palmPosition[2]) + "<br />";
+        frameOutput.innerHTML = "<div>" + osc_one + "</div>";
 
-            if (-100 < hand.palmPosition[0] && hand.palmPosition[0] < 100) {
-                if (20 < hand.palmPosition[1] && hand.palmPosition[1] < 60) {
-                    currentFreq = notes[4][0]; // play C0
-                    output += "<br />" + "C0"
-                }
-                else if (60 < hand.palmPosition[1] && hand.palmPosition[1] < 100) {
-                    currentFreq = notes[4][1]; // play D0
-                    output += "<br />" + "D0"
-                }
-                else if (100 < hand.palmPosition[1] && hand.palmPosition[1] < 140) {
-                    currentFreq = notes[4][2]; // play E0
-                    output += "<br />" + "E0"
-
-                }
-                else if (140 < hand.palmPosition[1] && hand.palmPosition[1] < 180) {
-                    currentFreq = notes[4][3]; // play F0
-                    output += "<br />" + "F0"
-                }
-                else if (180 < hand.palmPosition[1] && hand.palmPosition[1] < 220) {
-                    currentFreq = notes[4][4]; // play G0
-                    output += "<br />" + "G0"
-                }
-                else if (220 < hand.palmPosition[1] && hand.palmPosition[1] < 260) {
-                    currentFreq = notes[4][5]; // play G0
-                    output += "<br />" + "A0"
-                }
-                else if (260 < hand.palmPosition[1] && hand.palmPosition[1] < 300) {
-                    currentFreq = notes[4][6]; // play G0
-                    output += "<br />" + "B0"
-                }
-            }
-            console.log(notes[0][0]);
-            console.log(currentFreq);
-            primaryOsc.freq(currentFreq);
- 
+        if (frame.hands.length > 1) {
+            var hand_two = frame.hands[1];
+            var osc_two = handOscillator(output, hand_two, b_osc2, h_osc2, secondary_amp, secondary_freq);
+    
+            frameOutput.innerHTML += "<div>" + osc_two + "</div>";
 
         }
-        else {
-            primaryOsc.amp(0.0);
-        }
- 
-        
+
     }
     else {
-        primaryOsc.freq(currentFreq);
+        b_osc.amp(0);
+        h_osc.amp(0);
+        b_osc2.amp(0);
+        h_osc2.amp(0);
     }
 
-    frameOutput.innerHTML = "<div>" + output + "</div>";
-
 })
+
+function handOscillator(output, hand, osc, harmonic, amplitude, frequency) {
+
+    if (20 < hand.palmPosition[1] && hand.palmPosition[1] < 60) {
+        frequency = 261.63 // play C4
+        output += "<br />" + "<b>Playing</b> C4" + "<br />" + "Harmonic: " + frequency*2;
+    }
+    else if (60 < hand.palmPosition[1] && hand.palmPosition[1] < 100) {
+        frequency = 293.66 // play D4
+        output += "<br />" + "<b>Playing</b> D4" + "<br />" + "Harmonic: " + frequency*2;
+    }
+    else if (100 < hand.palmPosition[1] && hand.palmPosition[1] < 140) {
+        frequency = 329.63 // play E0
+        output += "<br />" + "<b>Playing</b> E4" + "<br />" + "Harmonic: " + frequency*2;
+
+    }
+    else if (140 < hand.palmPosition[1] && hand.palmPosition[1] < 180) {
+        frequency = 349.23; // play F0
+        output += "<br />" + "<b>Playing</b> F4" + "<br />" + "Harmonic: " + frequency*2;
+    }
+    else if (180 < hand.palmPosition[1] && hand.palmPosition[1] < 220) {
+        frequency = 392.00; // play G0
+        output += "<br />" + "<b>Playing</b> G4" + "<br />" + "Harmonic: " + frequency*2;
+    }
+    else if (220 < hand.palmPosition[1] && hand.palmPosition[1] < 260) {
+        frequency = 440.00; // play G0
+        output += "<br />" + "<b>Playing</b> A4" + "<br />" + "Harmonic: " + frequency*2;
+    }
+    else if (260 < hand.palmPosition[1] && hand.palmPosition[1] < 300) {
+        frequency = 493.88; // play G0
+        output += "<br />" + "<b>Playing</b> B4" + "<br />" + "Harmonic: " + frequency*2;
+    }
+    
+    osc.freq(frequency);
+    harmonic.freq(frequency*2);
+
+    amplitude = 1 - hand.grabStrength;
+
+    osc.amp(amplitude);
+    harmonic.amp(amplitude * Math.max(0, (hand.roll() / -1.57)));
+
+    output += "<br /><b>Hand Volume: </b>" + Math.round(100 * amplitude) + "%" + "<br />"
+
+    return output;
+    
+}
--- js/theremin.js
+++ js/theremin.js
@@ -1,87 +1,111 @@
-var output = null;
+var output = "";
 var leapOptions = { enableGestures: false };
 var frequency = 0;
 var primary_vol = 0.0;
 var harmonics_vol = 0.0;
 var handPosition;
+var max_freq = 493.88;
+var min_freq = 16.35; 
 var output;
 
 // primary oscillator
 var osc = new p5.Oscillator();
 osc.setType('sine');
 osc.amp(primary_vol)
+osc.start();
 
-// set of oscillators to perform fourier transform 
+// set of oscillators to perform fourier transform //
 var f_osc = new p5.Oscillator();
 f_osc.setType('sine');
 f_osc.amp(primary_vol);
+f_osc.start();
 
 var f_osc2 = new p5.Oscillator();
 f_osc2.setType('sine');
 f_osc2.amp(primary_vol);
+f_osc2.start();
 
 var f_osc3 = new p5.Oscillator();
 f_osc3.setType('sine');
 f_osc3.amp(primary_vol);
+f_osc3.start();
 
 var f_osc4 = new p5.Oscillator();
 f_osc4.setType('sine');
 f_osc4.amp(primary_vol);
-
-
-osc.start();
-f_osc.start();
-f_osc2.start();
-f_osc3.start();
 f_osc4.start();
+//////////////////////////////////////////////////////
+
+osc.disconnect()
+f_osc.disconnect()
+f_osc2.disconnect()
+f_osc3.disconnect()
+f_osc4.disconnect()
+
+// reverb processing for each oscillator
+reverb = new p5.Reverb();
+reverb.process(osc, 1, 15);
+reverb.process(f_osc, 1, 15);
+reverb.process(f_osc2, 1, 15);
+reverb.process(f_osc3, 1, 15);
+reverb.process(f_osc4, 1, 15);
 
 // main loop
 Leap.loop(leapOptions, function (frame) {
 
-    // Display Frame object data
+    // display Frame object data in UI
     frameOutput = document.getElementById("frameData");
 
-    output = "Frame ID: " + frame.id + "<br />"
-        + "Timestamp: " + frame.timestamp + " &micro;s<br />"
-    
-
+    // check if hand is present
     if (frame.hands.length > 0) {
 
         hand = frame.hands[0];
         handPosition = hand.palmPosition;
         
+        // set volume of each oscillator
         osc.amp(primary_vol); // master
-        f_osc.amp((harmonics_vol)/0.33); // third harmonic
-        f_osc2.amp((harmonics_vol)/0.20); // fifth harmonic
-        f_osc3.amp((harmonics_vol)/0.14); // seventh harmonic
-        f_osc4.amp((harmonics_vol)/0.11); // ninth harmonic
 
+        // the value each harmonic is multiplied by is the harmonic's relative amplitude to the master
+        f_osc.amp((harmonics_vol)/3); // third harmonic
+        f_osc2.amp((harmonics_vol)/5); // fifth harmonic
+        f_osc3.amp((harmonics_vol)/7); // seventh harmonic
+        f_osc4.amp((harmonics_vol)/9); // ninth harmonic
+
+        // determine if hand is in ideal range
         if (handPosition[1] > 350) {
+            // set the volume to 0 if it isn't
             primary_vol = 0.0;    
         }
+        // if the hand is in range...
         else {
 
-            // scale frequency into 4th octave (middle octave)
-            frequency = ((523.25 -  261.63) * handPosition[1]-30) / (300) +  261.63;
+            // scale user input between 0th and 6th octaves to determine base frequency
+            frequency = (max_freq -  min_freq) * ((handPosition[1]-25) / 325) +  min_freq;
+
             osc.freq(frequency);
+            
+            // set the frequencies of the odd harmonics
             f_osc.freq(frequency * 3);
             f_osc2.freq(frequency * 5);
             f_osc3.freq(frequency * 7);
             f_osc4.freq(frequency * 9);
         
 
-            primary_vol = 1-(hand.grabStrength);
+            // closing hand determines the volume 
+            primary_vol = 1 - hand.grabStrength;
+
+            // the initial volume of the harmonics is calculated based on hand rotation between 0 and 90 degrees
             harmonics_vol = primary_vol * Math.max(0, (hand.roll() / -1.57))
 
-            output += "<br />" + "Hand detected, play sound!" + "<br />"
-            // + "Primary hand position X: " + (handPosition[0]) + "<br />"
-            + "Hand position Y: " + (handPosition[1]) + "<br />"
-            // + "Primary hand position Z: " + (handPosition[2]) + "<br />"
-            + "Hand rotation angle: " + hand.roll() + "<br />"
-            + "Volume: " + (primary_vol) + "<br />"
-            + "Frequency: " + Math.round(frequency).toFixed(2) + "<br />";
+            // output information to the UI for the user
+            output = "<b>Master Volume: </b>" + Math.round(100 * primary_vol) + "%" + "<br />"
+            + "<b>Frequency: </b>" + Math.round(frequency) + "Hz" + "<br />"
+            + "<br />" + "<b>Harmonics</b>" + "<br />"
+            + "3rd harmonic: " + Math.round(frequency * 3) + "Hz" + "<br />"
+            + "5th harmonic: " + Math.round(frequency * 5) + "Hz" + "<br />"
+            + "7th harmonic: " + Math.round(frequency * 7) + "Hz" + "<br />"
+            + "9th harmonic: " + Math.round(frequency * 9) + "Hz" + "<br />";
         }
-
     }
     else {
         osc.amp(0); // master
